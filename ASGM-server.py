c4563dca01441d4d44bb0025d96b36fc976b345a
Sai Shibu <saishibu38380@gmail.com>
2018-07-12 12:02:08 +0530

    Update ASGM-server.py

--- ASGM-server.py
+++ ASGM-server.py
@@ -1,5 +1,5 @@
 #Import required libraries
-import  urllib2,json,time
+import  urllib2,json,time,pymysql
 
 
 #Define a page opener using inbuilt function from urllib2
@@ -16,3 +16,11 @@ json_api=json.loads(api)
 print 'voltage1: '+str(json_api['Recent data'][-1]['v1']) +'V'
 print 'voltage2: '+str(json_api['Recent data'][-1]['v2']) +'V'
 print 'voltage3: '+str(json_api['Recent data'][-1]['v3']) +'V'
+
+#insert to DB (requires pymysql package (pip install pymysql))
+conn =pymysql.connect(database="AmritaSGM",user="root",password="amma",host="localhost")
+cur=conn.cursor()
+cur.execute("INSERT INTO node_data(nodeid, METER,v1, v2, v3, i1, i2, i3, w1,w2,w3,va1,va2,va3,var1,var2,var3,wh,vah,varh,wh1,wh2,wh3,v$
+conn.commit()
+conn.close()
+print "DB Dump success"
