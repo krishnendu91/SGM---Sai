6726442138fbd821b8d6454653b50b531042e6c6
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-14 12:47:01 +0530

    Update rest_flash_sgm.py

--- rest_flash_sgm.py
+++ rest_flash_sgm.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 mysql = MySQL()
 urls=("/favicon.ico","dummy")
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
 app.config['MYSQL_DATABASE_DB'] = 'AmritaSGM'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
@@ -22,35 +22,35 @@ def welcome():
 @app.route('/recent')
 def recent():
 	cur = mysql.connect().cursor()
-	cur.execute('select * from node_data ORDER BY id DESC LIMIT 1')
+	cur.execute('select * from nodeData ORDER BY id DESC LIMIT 1')
 	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
 	return jsonify({'Recent data' : r})
 
 @app.route('/hour')
 def hhistory():
 	cur = mysql.connect().cursor()
-	cur.execute('select * from node_data where timestamp >= DATE_SUB(NOW(),INTERVAL 1 HOUR)')
+	cur.execute('select * from nodeData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 HOUR)')
 	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
 	return jsonify({'Last one hour' : r})
 
 @app.route('/day')
 def dhistory():
 	cur = mysql.connect().cursor()
-	cur.execute('select * from node_data where timestamp >= DATE_SUB(NOW(),INTERVAL 1 DAY)')
+	cur.execute('select * from nodeData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 DAY)')
 	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
 	return jsonify({'Last one day' : r})
 
 @app.route('/month')
 def mhistory():
         cur = mysql.connect().cursor()
-        cur.execute('select * from node_data where timestamp >= DATE_SUB(NOW(),INTERVAL 1 MONTH)')
+        cur.execute('select * from nodeData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 MONTH)')
         r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
         return jsonify({'Last one month' : r})
 
 @app.route('/year')
 def yhistory():
         cur = mysql.connect().cursor()
-        cur.execute('select * from node_data where timestamp >= DATE_SUB(NOW(),INTERVAL 1 YEAR)')
+        cur.execute('select * from nodeData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 YEAR)')
         r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
         return jsonify({'Last one year' : r})
 
