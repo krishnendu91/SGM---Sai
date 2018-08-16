097cba93b5f94eb2976e82047cc65343450483bf
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-16 10:10:37 +0530

    Update grabrest.py

--- Server/grabrest.py
+++ Server/grabrest.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 #Import required libraries
-import  urllib2,json,time,pymysql
+import json,time,pymysql
 import sys
-
+from urllib.request import urlopen
 #Define a page opener using inbuilt function from urllib2
-opener=urllib2.build_opener()
+#opener=urllib2.build_opener()
 
 #Define the url (http://<node ip>:5000/api)
 ip=sys.argv[1]
 url='http://'+str(ip)+':5000/recentGM'
 print (url)
 #Read the url and save to a variable
-api_page=opener.open(url)
-
+# api_page=opener.open(url) For Python 2
+api_page = urlopen(url) #Python 3
 #Read the api output from the variable 
 api=api_page.read()
 
