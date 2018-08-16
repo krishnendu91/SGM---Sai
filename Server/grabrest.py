110619310da695f516e9f523538ba913567b2aed
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-16 09:35:03 +0530

    Update grabrest.py

--- Server/grabrest.py
+++ Server/grabrest.py
@@ -9,7 +9,7 @@ opener=urllib2.build_opener()
 #Define the url (http://<node ip>:5000/api)
 ip=sys.argv[1]
 url='http://'+str(ip)+':5000/recentGM'
-
+print (url)
 #Read the url and save to a variable
 api_page=opener.open(url)
 
@@ -21,13 +21,13 @@ json_api=json.loads(api)
 data= json_api['Recent data'][-1]
 
 #Optional: Print required data
-print "Website grabbed"
-print data
+print ("Website grabbed")
+print (data)
 
 #insert to DB (Requires pymysql package) (install it by sudo pip pymysql)
-conn =pymysql.connect(database="AmritaSGM",user="root",password="amma",host="localhost")
-cur=conn.cursor()
-cur.execute("INSERT INTO server_data(nodeid, METER,v1, v2, v3, i1, i2, i3, w1,w2,w3,va1,va2,va3,var1,var2,var3,wh,vah,varh,wh1,wh2,wh3$
-conn.commit()
-conn.close()
-print "DB Dump success"
+#conn =pymysql.connect(database="AmritaSGM",user="root",password="amma",host="localhost")
+#cur=conn.cursor()
+#cur.execute("INSERT INTO server_data(nodeid, METER,v1, v2, v3, i1, i2, i3, w1,w2,w3,va1,va2,va3,var1,var2,var3,wh,vah,varh,wh1,wh2,wh3$
+#conn.commit()
+#conn.close()
+print ("DB Dump success")
