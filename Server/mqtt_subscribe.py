057eb27f50b6f3aae14a281e95a25234ff74076d
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-29 14:45:31 +0530

    Create mqtt_subscribe.py

---
+++ Server/mqtt_subscribe.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+# This is the Subscriber
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+	client.subscribe("topic/test")
+
+def on_message(client, userdata, msg):
+	#if msg.payload.decode() == "Hello world!":
+	status=msg.payload.decode()
+	#print(status)
+	data={'status':status}
+	print(data)
+  #todb(data)
+    #client.disconnect()
+    
+client = mqtt.Client()
+client.connect("0.0.0.0",1883,60)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever()
