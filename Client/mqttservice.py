3ee3129bd4f06523bb33935b59cdeecd28dbe43a
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-29 15:24:34 +0530

    Update mqttservice.py

--- Client/mqttservice.py
+++ Client/mqttservice.py
@@ -1,12 +1,14 @@
 import paho.mqtt.client as mqtt
-
-def mqtt_publish(broker,port,topic,payload)
+def mqtt_publish(broker,port,topic,payload,ip_wlan0):
 # This is the Publisher
   #broker="192.168.112.110"
   #port=1883
   #topic="switch"
   #payload="ON"
+  payload={'message':payload,"ip":ip_wlan0}
   topic="SGM/"+topic
+  print(topic)
+  print(payload)
   client = mqtt.Client()
   client.connect(broker,port,60)
   client.publish(topic,payload);
