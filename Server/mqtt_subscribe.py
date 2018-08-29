1a7230c9a490ca828c6040a6775dcc971087c28d
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-29 15:19:41 +0530

    Update mqtt_subscribe.py

--- Server/mqtt_subscribe.py
+++ Server/mqtt_subscribe.py
@@ -3,14 +3,12 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
-	client.subscribe("topic/test")
+	client.subscribe("SGM/datafetch")
 
 def on_message(client, userdata, msg):
 	#if msg.payload.decode() == "Hello world!":
 	status=msg.payload.decode()
-	#print(status)
-	data={'status':status}
-	print(data)
+	print(status)
   #todb(data)
     #client.disconnect()
     
