f33ca153489b037b9f5f7b0018025b5d939a9d06
saishibu38380 <saishibu38380@gmail.com>
2018-08-30 17:16:57 +0530

    rest for test

---
+++ Server/rest_flash_sgm.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+urls=("/favicon.ico","dummy")
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
+app.config['MYSQL_DATABASE_DB'] = 'AmritaSGM'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
+
+@app.route('/')
+def welcome():
+#	print "Welcome to Amrita Smart-Grid Middleware"
+#	print "kindly use one of the APIs to get data"
+	return "\tWelcome to Amrita Smart-Grid Middleware.\n\n \tKindly use one of the APIs to get data"
+
+
+@app.route('/recent')
+def recent():
+	cur = mysql.connect().cursor()
+	cur.execute('select * from nodeData where meterType=1 ORDER BY id DESC LIMIT 1 ')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	return jsonify({'Recent data' : r})
+
+@app.route('/recentlm1')
+def recentlm1():
+	cur = mysql.connect().cursor()
+	cur.execute('select * from nodeData where meterType=2 ORDER BY id DESC LIMIT 1')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	return jsonify({'Recent data' : r})
+
+@app.route('/recentlm2')
+def recentlm2():
+	cur = mysql.connect().cursor()
+	cur.execute('select * from nodeData where meterType=3 ORDER BY id DESC LIMIT 1')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	return jsonify({'Recent data' : r})
+
+@app.route('/outbackrecent')
+def outbackrecent():
+	cur = mysql.connect().cursor()
+	cur.execute('select * from inverterData ORDER BY id DESC LIMIT 1')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	return jsonify({'Recent data' : r})
+
+@app.route('/hour')
+def hhistory():
+	cur = mysql.connect().cursor()
+	cur.execute('select * from nodeData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 HOUR)')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	return jsonify({'Last one hour' : r})
+
+@app.route('/day')
+def dhistory():
+	cur = mysql.connect().cursor()
+	cur.execute('select * from nodeData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 DAY)')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	return jsonify({'Last one day' : r})
+
+@app.route('/month')
+def mhistory():
+        cur = mysql.connect().cursor()
+        cur.execute('select * from nodeData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 MONTH)')
+        r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+        return jsonify({'Last one month' : r})
+
+@app.route('/year')
+def yhistory():
+        cur = mysql.connect().cursor()
+        cur.execute('select * from nodeData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 YEAR)')
+        r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+        return jsonify({'Last one year' : r})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=7070,debug=1)
+
