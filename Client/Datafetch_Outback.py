8f46af9b5faa04d9d20e9dbe05164785e4173456
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-30 11:59:49 +0530

    Update Datafetch_Outback.py

--- Client/Datafetch_Outback.py
+++ Client/Datafetch_Outback.py
@@ -12,7 +12,7 @@ ip_wlan0=scanoutput.decode().split()[1]
 mate=urlopen('http://192.168.0.64/Dev_status.cgi?&Port=0')
 mate=mate.read()
 mate=json.loads(mate)
-print mate
+print(mate)
 
 mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch","DONE",ip_wlan0)
 print("MQTT Success")
