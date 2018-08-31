be6d92b1b485265f01b0bc6ba09029e626d630c6
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-31 17:22:54 +0530

    Update mqtt_reply.py

--- Client/mqtt_reply.py
+++ Client/mqtt_reply.py
@@ -8,10 +8,13 @@ def on_connect(client1, userdata, flags, rc):
 def on_message(client1, userdata, msg):
 	payload=msg.payload.decode()
 	print(payload)
+	client1.disconnect()
 def mqttack():
 	client1 = mqtt.Client()
 	client1.connect("0.0.0.0",1883,60)
 	#client.subscribe("SGM/ack")
-	client1.on_connect = on_connect
+	#client1.on_connect = on_connect
+	client1.subscribe("SGM/ack")
 	client1.on_message = on_message
 	print("ack received")
+	client1.loop_forever()
