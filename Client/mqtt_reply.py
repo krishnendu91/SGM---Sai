39e6d9de3a6fd1db122585d51b27114021613f4c
Sai Shibu N B <saishibu38380@gmail.com>
2018-08-31 17:27:42 +0530

    Update mqtt_reply.py

--- Client/mqtt_reply.py
+++ Client/mqtt_reply.py
@@ -1,20 +1,14 @@
 import paho.mqtt.client as mqtt
-
-
 #acknowledgement 
-def on_connect(client1, userdata, flags, rc):
-	print("Connected with result code "+str(rc))
-	client1.subscribe("SGM/ack")
+
 def on_message(client1, userdata, msg):
 	payload=msg.payload.decode()
 	print(payload)
+	print("ack received")
 	client1.disconnect()
 def mqttack():
 	client1 = mqtt.Client()
 	client1.connect("0.0.0.0",1883,60)
-	#client.subscribe("SGM/ack")
-	#client1.on_connect = on_connect
 	client1.subscribe("SGM/ack")
 	client1.on_message = on_message
-	print("ack received")
 	client1.loop_forever()
