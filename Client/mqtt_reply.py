0086da2a4bc2ad9066523b1e9082d5d88338a711
Sai Shibu N B <saishibu38380@gmail.com>
2018-09-01 14:20:59 +0530

    Update mqtt_reply.py

--- Client/mqtt_reply.py
+++ Client/mqtt_reply.py
@@ -1,10 +1,16 @@
 import paho.mqtt.client as mqtt
 #acknowledgement 
 
+
 def on_message(client1, userdata, msg):
 	payload=msg.payload.decode()
 	print(payload)
-	print("ack received")
+	if payload == "SUCCESS":
+		dbclear(time)
+		print("ack received")
+	else:
+		print("ack failed")
+		
 	client1.disconnect()
 def mqttack():
 	client1 = mqtt.Client()
@@ -12,3 +18,6 @@ def mqttack():
 	client1.subscribe("SGM/ack")
 	client1.on_message = on_message
 	client1.loop_forever()
+
+def dbclear:
+	print("hello")
