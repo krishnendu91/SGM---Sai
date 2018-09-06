ca0c190cc7467a4fe379aa8b2beea301de9f33ec
Sai Shibu N B <saishibu38380@gmail.com>
2018-09-06 10:18:26 +0530

    Update Datafetch_Outback.py

--- Client/Datafetch_Outback.py
+++ Client/Datafetch_Outback.py
@@ -14,5 +14,27 @@ mate=mate.read()
 mate=json.loads(mate)
 print(mate)
 
+inv1= r['devstatus']['ports'][0]
+inv2= r['devstatus']['ports'][1]
+inv3= r['devstatus']['ports'][2]
+cc1= r['devstatus']['ports'][3]
+cc2= r['devstatus']['ports'][4]
+cc3= r['devstatus']['ports'][5]
+
+print "inv1"
+print inv1
+print "inv2"
+print inv2
+print "inv3"
+print inv3
+print "cc1"
+print cc1
+print "cc2"
+print cc2
+print "cc3"
+print cc3
+
+
+
 mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch","DONE",ip_wlan0)
 print("MQTT Success")
