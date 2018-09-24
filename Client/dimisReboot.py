fc170d2c54f76a74723c7c013bcd361837fb23d2
Sai Shibu N B <saishibu38380@gmail.com>
2018-09-24 12:40:04 +0530

    Create dimisReboot.py

---
+++ Client/dimisReboot.py
@@ -0,0 +1,11 @@
+!#usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM) 
+GPIO.setwarnings(False) 
+GPIO.setup(17,GPIO.OUT) 
+
+GPIO.output(17,GPIO.HIGH)
+time.sleep(1)
+GPIO.output(17,GPIO.LOW)
