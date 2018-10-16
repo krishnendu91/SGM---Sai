b4341969db35adeb697f30db9f733f5f5cfec6d9
Sai Shibu N B <saishibu38380@gmail.com>
2018-10-16 14:51:54 +0530

    Update nodeHealth.py

--- Client/nodeHealth.py
+++ Client/nodeHealth.py
@@ -8,6 +8,9 @@ ip_scanoutput=check_output(["hostname -I"],shell=1)
 ip_eth0=ip_scanoutput.decode().split()[0]
 ip_wlan0=ip_scanoutput.decode().split()[1]
 
+temp=subprocess.check_output(["vcgencmd","measure_temp"])
+temp=temp.replace("temp=","").replace("'C\n","")
+
 #get node ID
 id_scanoutput=check_output(["hostname"],shell=1)
 id_node=id_scanoutput.decode().split()[0]
@@ -17,10 +20,10 @@ else:
 	nodeId=int(id_node[-1:])
 
 #Store to DB
-dataHealth={'nodeid':nodeId,'alive':1}
+dataHealth={'nodeid':nodeId,'alive':1,'temp':temp}
 conn =pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
 cur=conn.cursor()
-cur.execute("INSERT INTO nodeHealth(nodeid,alive) VALUES(%(nodeid)s,%(alive)s);",dataHealth)
+cur.execute("INSERT INTO nodeHealth(nodeid,alive,temp) VALUES(%(nodeid)s,%(alive)s,,%(temp)s);",dataHealth)
 conn.commit()
 conn.close()
 print ("DB Dump success")
