7a323270935a07659b3d04c000b89a370fdde55c
Sai Shibu N B <saishibu38380@gmail.com>
2018-10-25 14:38:00 +0530

    Update multi_MQTT.py

--- Server/multi_MQTT.py
+++ Server/multi_MQTT.py
@@ -11,18 +11,18 @@ global port;
 broker = "192.168.112.110";
 port = 1883;
 mypid = os.getpid()
-print(mypid)
+print("Process started at: " +str(mypid))
 client_uniq = "pubclient_"+str(mypid)
 mqttclient = paho.Client(client_uniq, False) #nocleanstart
 mqttclient.connect(broker, port, 60)
 mqttclient.subscribe("SGM/#")
 
 def test1(client, userdata, message):
-  print("test1")
-  print("Received message '" + str(message.payload) + "' on topic '" + message.topic + "' with QoS " + str(message.qos))
+	print("test1")
+	print("Received message '" + str(message.payload) + "' on topic '" + message.topic + "' with QoS " + str(message.qos))
   
 def datafetch(client, userdata, msg):
-  payload=json.loads(msg.payload.decode())
+	payload=json.loads(msg.payload.decode())
 	ip_wlan0 = payload['ip']
 	if payload['message'] == "DONE":
 		api='recentgm'
@@ -35,7 +35,7 @@ def datafetch(client, userdata, msg):
 		reply="FAIL"
 		print(reply)
 	
-  reply=json.dumps(reply)
+	reply=json.dumps(reply)
 	mqtt_reply.mqttack(ip_wlan0,reply)
   
 mqttclient.message_callback_add("SGM/test1", test1)
