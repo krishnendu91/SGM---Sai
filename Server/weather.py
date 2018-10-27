57eb993069967a0b0cb629b52024e0a0affee089
Sai Shibu N B <saishibu38380@gmail.com>
2018-10-27 09:34:25 +0530

    Update weather.py

--- Server/weather.py
+++ Server/weather.py
@@ -10,5 +10,5 @@ url="https://api.darksky.net/forecast/8636a0eb845f3447caa12fe33102d60f/9.092534,
 api_page = urlopen(url)
 api=api_page.read()
 json_api=json.loads(api)
-data= json_api['currently'][-1]
+data= json_api['currently']
 print(data)
