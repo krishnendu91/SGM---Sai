c480664c6cb11795db8b2d8d99dcd2888e2750b7
Sai Shibu N B <saishibu38380@gmail.com>
2018-10-29 13:13:54 +0530

    Update algorithm.py

--- Client/algorithm.py
+++ Client/algorithm.py
@@ -2,13 +2,13 @@ import pymysql,mqtt_reply,mqttservice
 conn =pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
 cur=conn.cursor()
 
-def poweroutage(v1,v2,v3,nodeId,ip_wlan0):
+def poweroutage(v1,v2,v3,meterID,nodeId,ip_wlan0):
   check = v1 or v2 or v3 
   print(check)
   if (check > 0):
-    data={'nodeId':nodeId,'errorId':1,'errorMsg':'no input voltage supply','errorVal':check}
+    data={'nodeId':nodeId,'errorId':1,'errorMsg':'no input voltage supply','errorVal':check,'meterID':meterID}
     print(data)
-    cur.execute("INSERT INTO event(`nodeId`, `errorId`, `errorMsg`, `errorVal`) VALUES (%(nodeId)s, %(errorId)s, %(errorMsg)s, %(errorVal)s);",data)
+    cur.execute("INSERT INTO event(`nodeId`, `errorId`, `errorMsg`, `errorVal`,meterID) VALUES (%(nodeId)s, %(errorId)s, %(errorMsg)s, %(errorVal)s),%(meterID)s;",data)
     print("Event table updated")
     mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_events","DONE",ip_wlan0)
     #return 
