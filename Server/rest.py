b525fc086e532385e971f32bec5045d549ad949a
Sai Shibu N B <saishibu38380@gmail.com>
2018-11-14 15:18:40 +0530

    Update rest.py

--- Server/rest.py
+++ Server/rest.py
@@ -90,7 +90,7 @@ def yhistory():
 @app.route('/dimis/event')
 def event():
         cur = mysql.connect().cursor()
-        cur.execute('select * from event where errorTime >= DATE_SUB(NOW(),INTERVAL 1 YEAR)')
+        cur.execute('select * from event where errorTime >= DATE_SUB(NOW(),INTERVAL 1 DAY)')
         r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
         return jsonify({'Event Data' : r})
 
