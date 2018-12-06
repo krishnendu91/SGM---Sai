fc4d46c8d1a648274dfd5fa06b1edc51e025b5e3
Sai Shibu N B <saishibu38380@gmail.com>
2018-12-06 10:06:55 +0530

    Update Datafetch_dimis.py

--- Client/Datafetch_dimis.py
+++ Client/Datafetch_dimis.py
@@ -88,7 +88,28 @@ def dimishelper(ip, port):
 #Read from GM1
 print (id_node)
 #if(1<id_node <9   | 9<id_node <12 | id_node ==13):
+
 if(id_node ==5):
+	#fetch GM
+	value_GM=dimishelper(ip_eth0,port_GM)
+	utils.todbdimis(value_GM)
+	mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_dimis_gm1","DONE",ip_wlan0)
+	algorithm.poweroutage(value_GM['v1'],value_GM['v2'],value_GM['v3'],value_GM['meterType'],id_node,ip_wlan0)
+	algorithm.frequency(value_GM['v1'],value_GM['v2'],value_GM['v3'],value_GM['meterType'],id_node,ip_wlan0)
+	algorithm.powerquality(value_GM['v1'],value_GM['v2'],value_GM['v3'],value_GM['meterType'],id_node,ip_wlan0)
+	algorithm.current(value_GM['i1'],value_GM['i2'],value_GM['i3'],value_GM['meterType'],id_node,ip_wlan0)
+	print("GM1 Fetch Complete")
+	#Fetch LM1
+	value_LM1=dimishelper(ip_eth0,port_LM1)
+	utils.todbdimis(value_LM1)
+	mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_dimis_lm1","DONE",ip_wlan0)
+	algorithm.poweroutage(value_LM1['v1'],value_LM1['v2'],value_LM1['v3'],value_LM1['meterType'],id_node,ip_wlan0)
+	algorithm.frequency(value_LM1['v1'],value_LM1['v2'],value_LM1['v3'],value_LM1['meterType'],id_node,ip_wlan0)
+	algorithm.powerquality(value_LM1['v1'],value_LM1['v2'],value_LM1['v3'],value_LM1['meterType'],id_node,ip_wlan0)
+	algorithm.current(value_LM1['i1'],value_LM1['i2'],value_LM1['i3'],value_LM1['meterType'],id_node,ip_wlan0)
+	print("LM1 Fetch Complete")
+
+elif(9<id_node <12):
 	#fetch GM
 	value_GM=dimishelper(ip_eth0,port_GM)
 	utils.todbdimis(value_GM)
