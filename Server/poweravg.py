7c53189b4e35ee3d7d0377c597f06295dace8842
Sai Shibu N B <saishibu38380@gmail.com>
2018-12-21 12:41:50 +0530

    Update poweravg.py

--- Server/poweravg.py
+++ Server/poweravg.py
@@ -28,7 +28,7 @@ def tdcalc(nodeId,dbtime):
 
 conn = pymysql.connect(database="AmritaSGM",user="grafana",password="grafana",host="localhost")
 cur=conn.cursor()
-cur.execute("SELECT timestamp,nodeid FROM `nodeHealth` where nodeid=1 order by id desc limit 1;")
+cur.execute("SELECT timestamp,nodeid FROM `nodeHealth` where nodeid=%s order by id desc limit 1;",1)
 data=cur.fetchone() #fetch all
 
 dbtime=data[0]
