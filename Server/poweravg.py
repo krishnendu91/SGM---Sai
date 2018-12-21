bcd710b0817b1d805fee2514c6079264768bca76
Sai Shibu N B <saishibu38380@gmail.com>
2018-12-21 12:49:39 +0530

    Update poweravg.py

--- Server/poweravg.py
+++ Server/poweravg.py
@@ -32,7 +32,7 @@ while(a<14):
     cur=conn.cursor()
     cur.execute("SELECT timestamp,nodeid FROM `nodeHealth` where nodeid=%s order by id desc limit 1;",a)
     data=cur.fetchone() #fetch all
-
+    print (data)
     dbtime=data[0]
     nodeid=data[1]
     tdcalc(nodeid,dbtime)
