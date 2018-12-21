b4e744b277f2bc444774da35493a717dd35fddf6
Sai Shibu N B <saishibu38380@gmail.com>
2018-12-21 13:04:29 +0530

    Update poweravg.py

--- Server/poweravg.py
+++ Server/poweravg.py
@@ -21,8 +21,8 @@ def tdcalc(nodeId,dbtime):
 
   conn = pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
   cur=conn.cursor()
-  data={"dbtime":dbtime,"alive":alive,"timedrift":timedrift,"nodeid":nodeid}
-  cur.execute("INSERT INTO `lastseen` (dbtime,alive,timedrift,nodeid) VALUES (%(dbtime)s,%(alive)s,%(timedrift)s,%(nodeid)s);",data)
+  data={"dbtime":dbtime,"alive":alive,"timedrift":timedrift,"nodeid":nodeid,"temp":temp,"ssid":ssid,"wlan_ss":wlan_ss}
+  cur.execute("INSERT INTO `lastseen` (dbtime,alive,timedrift,nodeid,temp,ssid,wlan_ss) VALUES (%(dbtime)s,%(alive)s,%(timedrift)s,%(nodeid)s,%(temp)s,%(ssid)s,%(wlan_ss)s);",data)
   conn.commit()
   conn.close()
   print("DB Updated with alive state: " +str(alive) + " for Node "+str(nodeid))
@@ -30,11 +30,15 @@ while(a<15):
   try:
     conn = pymysql.connect(database="AmritaSGM",user="grafana",password="grafana",host="localhost")
     cur=conn.cursor()
-    cur.execute("SELECT timestamp,nodeid FROM `nodeHealth` where nodeid=%s order by id desc limit 1;",a)
+    cur.execute("SELECT timestamp,nodeid,temp,ssid,wlan_ss FROM `nodeHealth` where nodeid=%s order by id desc limit 1;",a)
     data=cur.fetchone() #fetch all
     dbtime=data[0]
     nodeid=data[1]
-    tdcalc(nodeid,dbtime)
+    temp=data[2]
+    ssid=data[3]
+    wlan_ss=data[4]
+    
+    tdcalc(nodeid,dbtime,temp,ssid,wlan_ss)
     conn.close()
     a=a+1
   except:
