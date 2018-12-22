0b9ff5ed2694ece4e7594adf4afac782e1592f4e
Sai Shibu N B <saishibu38380@gmail.com>
2018-12-22 09:41:47 +0530

    Update Datafetch_Outback.py

--- Client/Datafetch_Outback.py
+++ Client/Datafetch_Outback.py
@@ -25,6 +25,7 @@ inv1['Error']=error
 inv1['Warn']=warn
 cur.execute("INSERT INTO inverterData(nodeid,type,port,battVoltage,aux,error,dev,vac1_in_l2,ac_input,vac_out_l2,inv_mode,inv_i_l2,warn,buy_i_l2,vac_in_l2,sell_i_l2,chg_i_l2,ac_mode) VALUES(%(nodeid)s,%(Type)s,%(Port)s,%(Batt_V)s,%(AUX)s,%(Error)s,%(Dev)s,%(VAC1_in_L2)s,%(AC_Input)s,%(VAC_out_L2)s,%(INV_mode)s,%(Inv_I_L2)s,%(Warn)s,%(Buy_I_L2)s,%(VAC2_in_L2)s,%(Sell_I_L2)s,%(Chg_I_L2)s,%(AC_mode)s);",inv1)
 conn.commit()
+mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_outback_inv","DONE",ip_wlan0)
 
 inv2= mate['devstatus']['ports'][3]
 inv2['nodeid']=14
@@ -34,6 +35,7 @@ inv2['Error']=error
 inv2['Warn']=warn
 cur.execute("INSERT INTO inverterData(nodeid,type,port,battVoltage,aux,error,dev,vac1_in_l2,ac_input,vac_out_l2,inv_mode,inv_i_l2,warn,buy_i_l2,vac_in_l2,sell_i_l2,chg_i_l2,ac_mode) VALUES(%(nodeid)s,%(Type)s,%(Port)s,%(Batt_V)s,%(AUX)s,%(Error)s,%(Dev)s,%(VAC1_in_L2)s,%(AC_Input)s,%(VAC_out_L2)s,%(INV_mode)s,%(Inv_I_L2)s,%(Warn)s,%(Buy_I_L2)s,%(VAC2_in_L2)s,%(Sell_I_L2)s,%(Chg_I_L2)s,%(AC_mode)s);",inv2)
 conn.commit()
+mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_outback_inv","DONE",ip_wlan0)
 
 inv3= mate['devstatus']['ports'][5] #5
 inv3['nodeid']=14
@@ -43,6 +45,7 @@ inv3['Error']=error
 inv3['Warn']=warn
 cur.execute("INSERT INTO inverterData(nodeid,type,port,battVoltage,aux,error,dev,vac1_in_l2,ac_input,vac_out_l2,inv_mode,inv_i_l2,warn,buy_i_l2,vac_in_l2,sell_i_l2,chg_i_l2,ac_mode) VALUES(%(nodeid)s,%(Type)s,%(Port)s,%(Batt_V)s,%(AUX)s,%(Error)s,%(Dev)s,%(VAC1_in_L2)s,%(AC_Input)s,%(VAC_out_L2)s,%(INV_mode)s,%(Inv_I_L2)s,%(Warn)s,%(Buy_I_L2)s,%(VAC2_in_L2)s,%(Sell_I_L2)s,%(Chg_I_L2)s,%(AC_mode)s);",inv3)
 conn.commit()
+mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_outback_inv","DONE",ip_wlan0)
 
 cc1= mate['devstatus']['ports'][1]
 cc1['nodeid']=14
@@ -50,6 +53,7 @@ error=cc1['Error'][0]
 cc1['Error']=error
 cur.execute("INSERT INTO inverterData(nodeid,type,port,battVoltage,aux,error,dev,cc_mode,aux_mode,in_i,out_i,in_v,out_kwh,out_ah ) VALUES(%(nodeid)s,%(Type)s,%(Port)s,%(Batt_V)s,%(AUX)s,%(Error)s,%(Dev)s,%(CC_mode)s,%(Aux_mode)s,%(In_I)s,%(Out_I)s,%(In_V)s,%(Out_kWh)s,%(Out_AH)s);",cc1)
 conn.commit()
+mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_outback_cc","DONE",ip_wlan0)
 
 cc2= mate['devstatus']['ports'][2]
 cc2['nodeid']=14
@@ -57,6 +61,7 @@ error=cc2['Error'][0]
 cc2['Error']=error
 cur.execute("INSERT INTO inverterData(nodeid,type,port,battVoltage,aux,error,dev,cc_mode,aux_mode,in_i,out_i,in_v,out_kwh,out_ah ) VALUES(%(nodeid)s,%(Type)s,%(Port)s,%(Batt_V)s,%(AUX)s,%(Error)s,%(Dev)s,%(CC_mode)s,%(Aux_mode)s,%(In_I)s,%(Out_I)s,%(In_V)s,%(Out_kWh)s,%(Out_AH)s);",cc2)
 conn.commit()
+mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_outback_cc","DONE",ip_wlan0)
 
 cc3= mate['devstatus']['ports'][4]
 cc3['nodeid']=14
@@ -65,6 +70,6 @@ cc3['Error']=error
 cur.execute("INSERT INTO inverterData(nodeid,type,port,battVoltage,aux,error,dev,cc_mode,aux_mode,in_i,out_i,in_v,out_kwh,out_ah ) VALUES(%(nodeid)s,%(Type)s,%(Port)s,%(Batt_V)s,%(AUX)s,%(Error)s,%(Dev)s,%(CC_mode)s,%(Aux_mode)s,%(In_I)s,%(Out_I)s,%(In_V)s,%(Out_kWh)s,%(Out_AH)s);",cc3)
 conn.commit()
 conn.close()
+mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_outback_cc","DONE",ip_wlan0)
 print("DB Dump success")
-#mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch","DONE",ip_wlan0)
 print("MQTT Success")
