c5cbd824cc5ff359c2bebe0e4c624579ab2cd0e1
Sai Shibu N B <saishibu38380@gmail.com>
2018-12-31 12:50:03 +0530

    Update invAggKwh.py

--- Server/invAggKwh.py
+++ Server/invAggKwh.py
@@ -4,7 +4,8 @@ import pymysql
 conn =pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
 cur=conn.cursor()
 cur.execute("SELECT out_kwh from inverterData where port=2 or 3 or 5 order by id desc limit 3")
-data=cur.fetchone()
+data=cur.fetchall()
+print(data)
 kwh1=data[0]
 kwh2=data[1]
 kwh3=data[2]
