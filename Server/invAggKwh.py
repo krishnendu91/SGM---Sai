c98c8bc62bfb8669be38f1ff3bfef23153225a63
Sai Shibu N B <saishibu38380@gmail.com>
2018-12-31 12:51:03 +0530

    Update invAggKwh.py

--- Server/invAggKwh.py
+++ Server/invAggKwh.py
@@ -6,9 +6,10 @@ cur=conn.cursor()
 cur.execute("SELECT out_kwh from inverterData where port=2 or 3 or 5 order by id desc limit 3")
 data=cur.fetchall()
 print(data)
-kwh1=data[0]
-kwh2=data[1]
-kwh3=data[2]
+for item in data:
+  kwh1=data[0]
+  kwh2=data[1]
+  kwh3=data[2]
 kwh=kwh1+kwh2+kwh3
 print(kwh)
 cur.execute("Insert into inverterData (totalKwh) values(%s);",kwh)
