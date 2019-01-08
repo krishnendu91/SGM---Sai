8c4179c5d1c71b5d0df7fbe99e5ad292eb6fdfc7
saishibu38380 <saishibu38380@gmail.com>
2019-01-08 15:25:15 +0530

    updated AGG

---
+++ AGG/mqttservice.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+import json,mqtt_reply
+
+def mqtt_publish(broker,port,topic,payload,ip_wlan0):
+# This is the Publisher
+  #broker="192.168.112.110"
+  #port=1883
+  #topic="switch"
+  #payload="ON"
+	payload={'message':payload,"ip":ip_wlan0}
+	payload=json.dumps(payload)
+	topic="SGM/"+topic
+	client = mqtt.Client()
+	client.connect(broker,port,60)
+	(rc,mid)=client.publish(topic,payload);
+	client.disconnect();
+	mqtt_reply.mqttack()
