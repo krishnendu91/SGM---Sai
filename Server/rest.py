be060911070aa600789c9901a66fd6abeb52f834
Sai Shibu N B <saishibu38380@gmail.com>
2019-01-10 10:38:21 +0530

    Update rest.py

--- Server/rest.py
+++ Server/rest.py
@@ -464,6 +464,15 @@ def outbackcc():
 	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
 	return jsonify({'Recent data' : r})
 
+@app.route('/sch')
+def sch():
+	cur = mysql.connect().cursor()
+	cur.execute('select * from schData ORDER BY id DESC LIMIT 1 ')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	r=jsonify({'Recent data' : r})
+	print(r{'timestamp'})
+	return
+
 
 
 
