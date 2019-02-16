dbd72e04ab735e580aeb356e3086785afe029a44
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-16 17:17:10 +0530

    Update rest_flash_sgm.py

--- Client/rest_flash_sgm.py
+++ Client/rest_flash_sgm.py
@@ -14,11 +14,49 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 mysql.init_app(app)
 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM) 
+GPIO.setwarnings(False) 
+
+pins = {26 : {'name' : 'R1', 'state' : GPIO.LOW},
+	19 : {'name' : 'R2', 'state' : GPIO.LOW},
+	13 : {'name' : 'R3', 'state' : GPIO.LOW},
+	6 : {'name' : 'R4', 'state' : GPIO.LOW},
+	12 : {'name' : 'R5', 'state' : GPIO.LOW},
+	16 : {'name' : 'R6', 'state' : GPIO.LOW},
+	20 : {'name' : 'R7', 'state' : GPIO.LOW},
+	21 : {'name' : 'R8', 'state' : GPIO.LOW}
+   }
+
+pinsID = {'R1' : {'gpio' : '26', 'state' : GPIO.LOW},
+	'R2' : {'gpio' : '19', 'state' : GPIO.LOW},
+	'R3' : {'gpio' : '13', 'state' : GPIO.LOW},
+	'R4': {'gpio' : '6', 'state' : GPIO.LOW},
+	'R5' : {'gpio' : '12', 'state' : GPIO.LOW},
+	'R6' : {'gpio' : '16', 'state' : GPIO.LOW},
+	'R7' : {'gpio' : '20', 'state' : GPIO.LOW},
+	'R8' : {'gpio' : '21', 'state' : GPIO.LOW}
+   }
+
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.LOW)
+
 @app.route('/')
 def welcome():
-#	print "Welcome to Amrita Smart-Grid Middleware"
-#	print "kindly use one of the APIs to get data"
 	return "\tWelcome to Amrita Smart-Grid Middleware.\n\n \tKindly use one of the APIs to get data"
+
+@app.route('/<pinId>')
+def action(pinId):
+	#state=int(state)
+	device=pinsID[pinId]['gpio']
+	print(device)
+	#print(pinsID[pinId]['state'])
+	GPIO.output(int(device),GPIO.HIGH)
+	time.sleep(0.3)
+	GPIO.output(int(device),GPIO.LOW)
+	return (str(device)+ " Activated")
 
 @app.route('/alive')
 def alive():
