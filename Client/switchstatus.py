bfa945c88deaae328c2c5ad9c7f5af456d216047
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-19 11:51:48 +0530

    Update switchstatus.py

--- Client/switchstatus.py
+++ Client/switchstatus.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
 import utils,mqttservice
+ip_eth0,ip_wlan0,id_node=utils.sysinfo()
 utils.switchstatus()
 mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_switch","DONE",ip_wlan0)
