70a4636438e256635eebfc3cf08bf0617829c26a
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-19 14:17:59 +0530

    Update utils.py

--- Client/utils.py
+++ Client/utils.py
@@ -6,7 +6,7 @@ import pymysql
 from subprocess import check_output
 
 def switchrest():
-  ip_eth0,ip_wlan0,id_node=utils.sysinfo()
+  ip_eth0,ip_wlan0,id_node=sysinfo()
   switchstatus()
   mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_switch","DONE",ip_wlan0)
   print("Switch State updated")
