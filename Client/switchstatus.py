de2fe08c14a431e5ca6fd8a71671dc11bfcd73be
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-19 11:53:37 +0530

    Update switchstatus.py

--- Client/switchstatus.py
+++ Client/switchstatus.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import utils,mqttservice
-ip_eth0,ip_wlan0,id_node=utils.sysinfo()
-utils.switchstatus()
-mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_switch","DONE",ip_wlan0)
+def switchrest():
+  ip_eth0,ip_wlan0,id_node=utils.sysinfo()
+  utils.switchstatus()
+  mqttservice.mqtt_publish("192.168.112.110",1883,"datafetch_switch","DONE",ip_wlan0)
+  print("Switch State updated")
