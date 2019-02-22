c7b4dbf4c7cda57c04ba8cf4de79720abb36d314
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-22 09:03:02 +0530

    Update restartDimis.sh

--- Client/restartDimis.sh
+++ Client/restartDimis.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 {
 service mosquitto restart
-service SGM_Client_api restart
+#service SGM_Client_api restart
 service datafetch restart
 echo "Service Restart Complete"
-}>> /home/pi/SGM_log/restartDimis.log
+} >> /home/pi/SGM_log/restartDimis.log
