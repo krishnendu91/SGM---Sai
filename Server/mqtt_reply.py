e92fa26ecdf9c48234eebf23cea56baaddfc43ce
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-22 16:41:30 +0530

    Update mqtt_reply.py

--- Server/mqtt_reply.py
+++ Server/mqtt_reply.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 
 def mqttack(broker,reply):
 	client1 = mqtt.Client()
-	client1.connect(broker,1883,600)
+	client1.connect(broker,1883,60)
 	client1.publish("SGM/ack",reply);
 	client1.disconnect()
 	print('acksent')
