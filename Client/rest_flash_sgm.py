7d325dd5917f2af7f6ad626889da4a385e96a8c0
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-23 17:22:49 +0530

    Update rest_flash_sgm.py

--- Client/rest_flash_sgm.py
+++ Client/rest_flash_sgm.py
@@ -78,12 +78,6 @@ def action(pinId):
 	utils.switchrest()
 	return (str(device)+ " Activated")
 
-@app.route('/deadnodes')
-def deadnodes():
-	cur = mysql.connect().cursor()
-	cur.execute('SELECT nodeId FROM `lastseen` WHERE alive =0 and timestamp>= NOW() - INTERVAL 3 MINUTE ORDER BY `id` DESC')
-	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
-	return jsonify({'Dead Nodes' : r})
 
 
 @app.route('/maxim')
