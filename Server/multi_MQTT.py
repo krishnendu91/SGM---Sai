f98e606f2d52d016717e281187f958971f6c1dc4
Sai Shibu N B <saishibu38380@gmail.com>
2019-02-25 13:11:37 +0530

    Update multi_MQTT.py

--- Server/multi_MQTT.py
+++ Server/multi_MQTT.py
@@ -50,6 +50,7 @@ def test(client, userdata, message):
 def datafetch_dimis_gm1_direct(client, userdata, msg):
 	print("Direct MQTT Message received")
 	payload=json.loads(msg.payload.decode())
+	print(payload)
 	#grabrest.todb(payload,1)
 		
 def datafetch_dimis_gm1(client, userdata, msg):
