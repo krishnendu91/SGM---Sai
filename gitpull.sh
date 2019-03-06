9ebdb602ef53649e3a56edae4253c27f7fd8c2cd
saishibu38380 <saishibu38380@gmail.com>
2019-03-06 13:59:41 +0530

    Updated

--- gitpull.sh
+++ gitpull.sh
@@ -1,12 +1,12 @@
 echo "Update process begins..."
 echo "#############################################"
-sleep 1
+sleep 0.3
 echo "Please be patient...."
 
 {
 cd /home/cs/SGM/
 git pull
-sleep 1
+sleep 0.3
 } >> /home/cs/updatelogs/github_pull.log
 echo "Github update completed"
 echo "updating local repository"
@@ -15,7 +15,8 @@ chmod 777 /home/cs/SGM/Client/*
 chmod 777 /home/cs/SGM/AGG/*
 cp /home/cs/SGM/Client/* /home/cs/SGM_Local/Client/
 cp /home/cs/SGM/AGG/* /home/cs/SGM_Local/AGG/
-sleep 1
+cp /home/cs/SGM/Client/STP/* /home/cs/SGM_Local/Client/STP/
+sleep 0.2
 } >> /home/cs/updatelogs/local_copy.log
 
 echo "copy to local repository completed"
@@ -23,11 +24,11 @@ echo "copy to local repository completed"
 cd /home/cs/SGM_Local/
 git add .
 git commit -m "auto updated"
-sleep 1
+sleep 0.3
 echo "Local commit success"
 } >> /home/cs/updatelogs/local_update.log
 
 echo "Updation Process Complete." 
-sleep 1
+sleep 0.3
 echo "Use git pull on client"
 echo "Bye-Bye!"
