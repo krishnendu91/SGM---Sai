35fd6eede1ad7152e7070af5bf61c8262e2444e2
Sai Shibu N B <saishibu38380@gmail.com>
2019-03-25 11:25:44 +0530

    Update utils.py

--- Client/STP/utils.py
+++ Client/STP/utils.py
@@ -27,7 +27,7 @@ def sysinfo():
 def todbsch(schData):
 	conn =pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
 	cur=conn.cursor()
-	cur.execute("INSERT INTO schData (nodeId,meterId,A, A1, A2, A3, VLL, VLN, V1, V2, V3, V12, V23, V31, F, PF, PF1, PF2, PF3, W, W1, W2, W3, VA, VA1, VA2, VA3, WH, VAH, INTR) VALUES (%(nodeId)s,%(meterId)s ,%(A)s, %(A1)s, %(A2)s, %(A3)s, %(VLL)s, %(VLN)s, %(V1)s, %(V2)s, %(V3)s, %(V12)s, %(V23)s, %(V31)s, %(F)s, %(PF)s, %(PF1)s, %(PF2)s, %(PF3)s, %(W)s, %(W1)s, %(W2)s, %(W3)s, %(VA)s, %(VA1)s, %(VA2)s, %(VA3)s, %(WH)s, %(VAH)s, %(INTR)s);",schData)
+	cur.execute("INSERT INTO schData (nodeId,meterId,meterName,A, A1, A2, A3, VLL, VLN, V1, V2, V3, V12, V23, V31, F, PF, PF1, PF2, PF3, W, W1, W2, W3, VA, VA1, VA2, VA3, WH, VAH, INTR) VALUES (%(nodeId)s,%(meterId)s,%(meterName)s,%(A)s, %(A1)s, %(A2)s, %(A3)s, %(VLL)s, %(VLN)s, %(V1)s, %(V2)s, %(V3)s, %(V12)s, %(V23)s, %(V31)s, %(F)s, %(PF)s, %(PF1)s, %(PF2)s, %(PF3)s, %(W)s, %(W1)s, %(W2)s, %(W3)s, %(VA)s, %(VA1)s, %(VA2)s, %(VA3)s, %(WH)s, %(VAH)s, %(INTR)s);",schData)
 	conn.commit()
 	conn.close()
 	print ("Schneider Meter - DB Dump success")
