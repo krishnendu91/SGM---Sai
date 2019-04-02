70d7958ccf4aa1be7dca7188f52ee8779b003ec6
Sai Shibu N B <saishibu38380@gmail.com>
2019-04-02 10:04:14 +0530

    Update XLGen.py

--- Server/XLGen.py
+++ Server/XLGen.py
@@ -6,7 +6,7 @@ from xlsxwriter.workbook import Workbook
 
 conn = pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
 cur=conn.cursor()
-cur.execute('SELECT * FROM STPData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 DAY);')
+cur.execute('SELECT * FROM STPData where timestamp >= DATE_SUB(NOW(),INTERVAL 1 DAY limit 5);')
 r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
 print(r)
 r=json.dumps(r)
