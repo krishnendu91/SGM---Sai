d339538b2f96c913d80280120a70767812b59d4d
Sai Shibu N B <saishibu38380@gmail.com>
2019-04-03 12:19:06 +0530

    Update STPHour.py

--- Server/STPHour.py
+++ Server/STPHour.py
@@ -11,7 +11,8 @@ while a<13:
 	meterName=cur.fetchone()
 	meterName=meterName[0]
 	cur.execute('SELECT A,VLL,W,F,PF,max(WH),timestamp FROM STPData WHERE meterName=%s ORDER by id desc limit 1',meterName)
-	data=cur.fetchall()
+	data=cur.fetchone()
+	print(data)
 	A=data[0]
 	VLL=data[1]
 	W=data[2]
