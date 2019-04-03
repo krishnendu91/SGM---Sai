e49fdbc76db550300584f87abbc6e7bbd3f63c5f
Sai Shibu N B <saishibu38380@gmail.com>
2019-04-03 12:17:10 +0530

    Update STPHour.py

--- Server/STPHour.py
+++ Server/STPHour.py
@@ -7,6 +7,25 @@ conn = pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="
 cur = conn.cursor()
 
 while a<13:
-cur.execute('SELECT meterName FROM STP where id= %(a)s ;',{'a':a})
-  meterName=cur.fetchone()
-  meterName=meterName[0]
+	cur.execute('SELECT meterName FROM STP where id= %(a)s ;',{'a':a})
+	meterName=cur.fetchone()
+	meterName=meterName[0]
+	cur.execute('SELECT A,VLL,W,F,PF,max(WH),timestamp FROM STPData WHERE meterName=%s ORDER by id desc limit 1',meterName)
+	data=cur.fetchall()
+	A=data[0]
+	VLL=data[1]
+	W=data[2]
+	F=data[3]
+	PF=data[4]
+	WH=data[5]
+	dbtime=data[6]
+	cur.execute('SELECT max(WH),timestamp FROM STPData WHERE meterName=%s and DATE_SUB(`timestamp`,INTERVAL 1 HOUR) ORDER by id desc limit 1',meterName)
+	data=cur.fetchall()
+	WH_old=data[0]
+	dbtime_old=data[1]
+	print(WH,dbtime)
+	print(WH_old,dbtime_old)
+	WH_new=WH-WH_old
+	print(WH)
+	a=a+1
+	
