ca379d2bf3fd09a354a64c54fa577f782e9d05bb
Sai Shibu N B <saishibu38380@gmail.com>
2019-04-03 09:21:09 +0530

    Update XLGen.py

--- Server/XLGen.py
+++ Server/XLGen.py
@@ -26,7 +26,7 @@ while a<13:
   meterName=cur.fetchone()
   meterName=meterName[0]
   print("Report Generating for "+str(meterName))
-  cur.execute('SELECT meterName,timestamp,VLL ,A, PF, F, W, Wh  FROM STPData where meterName = %s and timestamp >= DATE_SUB(NOW(),INTERVAL 1 HOUR) order by id desc;',meterName)
+  cur.execute('SELECT meterName,timestamp,VLL ,A, PF, F, W, Wh  FROM STPData where meterName = %(meterName)s and timestamp >= DATE_SUB(NOW(),INTERVAL 1 HOUR) order by id desc;',{'meterName':meterName})
   results = cur.fetchall()
   ws = wb.active
   ws.title = "STP Report - " + str(meterName)
@@ -79,16 +79,16 @@ s = smtplib.SMTP('smtp.amrita.ac.in', 25)
 s.starttls() 
 
 # Authentication 
-#s.login('noreply@amrita.ac.in','Amrita@123') 
+s.login('noreply@amrita.ac.in','Amrita@123') 
 
 # Converts the Multipart msg into a string 
 text = msg.as_string() 
 
 # sending the mail 
-#s.sendmail(fromaddr, toaddr1, text) 
+s.sendmail(fromaddr, toaddr1, text) 
 #s.sendmail(fromaddr, toaddr2, text) 
 
 # terminating the session 
 s.quit() 
 print("Email Sent")
-#delete=check_output(["rm *.xlsx"],shell=1)
+delete=check_output(["rm *.xlsx"],shell=1)
