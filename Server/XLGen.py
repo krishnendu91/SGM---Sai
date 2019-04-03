60b2ee2de428603fa71415ebd460c8a0a6b94bf8
Sai Shibu N B <saishibu38380@gmail.com>
2019-04-03 09:17:17 +0530

    Update XLGen.py

--- Server/XLGen.py
+++ Server/XLGen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import pymysql,json
-# mysql.connector as
+import mysql.connector as pymysql,json
+
 from openpyxl import Workbook
 from datetime import datetime
 import smtplib,sys 
