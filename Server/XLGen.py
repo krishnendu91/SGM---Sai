2d68a5e0b08849140f24d781229cf1a03f893979
Sai Shibu N B <saishibu38380@gmail.com>
2019-04-03 09:18:04 +0530

    Update XLGen.py

--- Server/XLGen.py
+++ Server/XLGen.py
@@ -22,7 +22,7 @@ cur = conn.cursor()
 
 wb = Workbook()
 while a<13:
-  cur.execute('SELECT meterName FROM STP where id= %d ;',a)
+  cur.execute('SELECT meterName FROM STP where id= %s ;',str(a))
   meterName=cur.fetchone()
   meterName=meterName[0]
   print("Report Generating for "+str(meterName))
