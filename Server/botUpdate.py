e7c136573f828743b8cbb6ad0588b7160c29745e
Sai Shibu N B <saishibu38380@gmail.com>
2019-04-10 14:49:56 +0530

    Update botUpdate.py

--- Server/botUpdate.py
+++ Server/botUpdate.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-import telegram
+import telegram,pymysql
 bot = telegram.Bot(token='813728431:AAEmpmT-UXIQQcdzwkih8k1XSdCbiMIFP2Q')
 chat_id =-304438902
-
-def sendmessage(message):
-  bot.send_message(chat_id=chat_id, text="Power Outage!")
-  return "Message Sent"
+bot.send_message(chat_id=chat_id, text="Power Outage!")
