38f1afaefe05a866b860efb52e85514edc364024
saishibu38380 <saishibu38380@gmail.com>
2019-05-01 10:20:07 +0530

    img

---
+++ Server/DBDump.sh
@@ -0,0 +1,2 @@
+#bin/bash
+mysqldump AmritaSGM > AmritaSGM.sql
--- Server/rest.py
+++ Server/rest.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3.7
 
-import datetime,sys,os
+import datetime,sys,os,io
 from urllib.request import urlopen
 from flask import Flask, jsonify,request
 from flaskext.mysql import MySQL
+import matplotlib.pyplot as plt
+import base64
+
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -45,6 +48,21 @@ def security(fname):
 	cur=conn.cursor()
 	cur.execute('INSERT INTO APILogs(clientAgent,clientIP,API)VALUES(%(clientAgent)s,%(clientIP)s,%(API)s); ',APILog)
 	conn.commit()
+
+@app.route('/plot')
+def build_plot():
+
+    img = io.BytesIO()
+
+    y = [1,2,3,4,5]
+    x = [0,2,1,3,4]
+    plt.plot(x,y)
+    plt.savefig(img, format='png')
+    img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    return '<img src="data:image/png;base64,{}">'.format(plot_url)
 
 @app.route('/')
 def welcome():
