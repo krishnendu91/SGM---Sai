99526f7478bd1dc0f518c75dfacdae664178fb32
Sai Shibu N B <saishibu38380@gmail.com>
2019-05-02 09:25:13 +0530

    Update errornotify.py

--- Server/errornotify.py
+++ Server/errornotify.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3
+import telegram,pymysql
+bot = telegram.Bot(token='813728431:AAEmpmT-UXIQQcdzwkih8k1XSdCbiMIFP2Q')
 
+def sendmessage(message):
+	chat_id =337874213
+	bot.send_message(chat_id=chat_id, text=message)
+	return "Done!"
