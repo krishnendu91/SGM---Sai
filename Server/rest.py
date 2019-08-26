d7ff94507fa7afb1566ea5b219a98643f1918fe0
Sai Shibu N B <saishibu38380@gmail.com>
2019-08-26 17:33:16 +0530

    Update rest.py

--- Server/rest.py
+++ Server/rest.py
@@ -440,6 +440,15 @@ def sch():
 	r[0]['timestampEpoch']=r[0]['timestamp'].timestamp()*1000
 	return jsonify({'Recent data' : r})
 
+@app.route('/ambtemp')
+def ambtemp():
+	security(str(sys._getframe().f_code.co_name))
+	cur = mysql.connect().cursor()
+	cur.execute('SELECT * FROM `tempData` ORDER BY `tempData`.`id` DESC LIMIT 3')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	r[0]['timestampEpoch']=r[0]['timestamp'].timestamp()*1000
+	return jsonify({'Recent temp' : r})
+
 if __name__ == '__main__':
 	app.run(host="0.0.0.0",port=5000,debug=1)
 
