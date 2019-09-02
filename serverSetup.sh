bf37d7e9af1bcb7830adc2c1617b0a6b21e8ece9
Sai Shibu N B <saishibu38380@gmail.com>
2019-09-02 15:29:31 +0530

    Create serverSetup.sh

---
+++ serverSetup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+apt update
+apt upgrade
+
+apt install python3
+apt install python3-pip
+
+apt install apache2
+apt install mysql-server
+apt install phpmyadmin
+
+pip3 install flask
+pip3 install pymysql
+pip3 install flask-mysql
+
+
