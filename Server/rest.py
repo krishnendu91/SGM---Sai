fb6a4384e550d0543c3520283a6a96f7b527dbde
Sai Shibu N B <saishibu38380@gmail.com>
2019-10-17 16:46:15 +0530

    Update rest.py

--- Server/rest.py
+++ Server/rest.py
@@ -63,7 +63,7 @@ def ambtemp():
 	security(str(sys._getframe().f_code.co_name))
 	conn = pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
 	cur = conn.cursor(pymysql.cursors.DictCursor)
-	cur.execute(SELECT * FROM tempData ORDER BY id DESC limit 1)
+	cur.execute("SELECT * FROM tempData ORDER BY id DESC limit 1")
 	data=cur.fetchall()
 	return jsonify({'Temperature' : data})
 
