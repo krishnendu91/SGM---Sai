19321aa74c232257497565d3842fd44735970741
Sai Shibu N B <saishibu38380@gmail.com>
2022-07-08 09:52:17 +0530

    Update multi_MQTT.py

--- Server/multi_MQTT.py
+++ Server/multi_MQTT.py
@@ -141,6 +141,15 @@ def ward(client, userdata, msg,):
 # 	conn1.commit()
 # 	conn1.close()
 
+def faclon(client, userdata, msg,):
+# 	conn1 =pymysql.connect(database="OceanNet",user="admin",password="admin",host="localhost")
+# 	cur1=conn1.cursor()
+	payload=json.loads(msg.payload.decode())
+	print(payload)
+# 	cur1.execute("INSERT INTO piggyback(TIME,boat,dir,ping_ms,ss,nf,rssi,pos,ccq,d,txrate,rxrate,freq,channel,bs_ip) VALUES(%(TIME)s,%(boat,%(dir)s,%(ping_ms)s,%(ss)s,%(nf)s,%(rssi)s,%(pos)s,%(ccq)s,%(d)s,%(txrate)s,%(rxrate)s,%(freq)s,%(channel)s,%(bs_ip)s);",payload)
+# 	conn1.commit()
+# 	conn1.close()
+
 def on_log(client, userdata, level, buf):
 	print("log:",buf)	
 	conn1 =pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
@@ -175,6 +184,7 @@ mqttclient.message_callback_add("SGM/temperature", temperature)
 mqttclient.message_callback_add("SGM/powerstate", powerstate)
 mqttclient.message_callback_add("SGM/onpiggyback", onpiggyback)
 mqttclient.message_callback_add("4ward/d1", ward)
+mqttclient.message_callback_add("faclon/dev", faclon)
 
 
 
