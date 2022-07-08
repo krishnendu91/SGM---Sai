f3dab49627d14240081978124f7dbbf18629f5d0
Sai Shibu N B <saishibu38380@gmail.com>
2022-07-08 10:23:15 +0530

    Update newMQTT4Ward.py

--- Server/newMQTT4Ward.py
+++ Server/newMQTT4Ward.py
@@ -1,12 +1,20 @@
 import paho.mqtt.client as mqtt #import the client1
-broker_address="0.0.0.0" 
-#broker_address="iot.eclipse.org" #use external broker
-client = mqtt.Client("P1") #create new instance
-client.connect(broker_address) #connect to broker
-client.subscribe("SGM/faclon/dev")
-
+import time
+############
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
+########################################
+broker_address="localhost"
+#broker_address="iot.eclipse.org"
+print("creating new instance")
+client = mqtt.Client("P1") #create new instance
+client.on_message=on_message #attach function to callback
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+client.loop_start() #start the loop
+print("Subscribing to topic","SGM/faclon/dev")
+client.subscribe("SGM/faclon/dev")
+client.loop_stop() #stop the loop
