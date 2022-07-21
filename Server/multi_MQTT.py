751692479c901d799842bb8666bb8084fa5116ad
Sai Shibu N B <saishibu38380@gmail.com>
2022-07-21 17:01:08 +0530

    Update multi_MQTT.py

--- Server/multi_MQTT.py
+++ Server/multi_MQTT.py
@@ -146,8 +146,9 @@ def wiman(client, userdata, msg,):
 def faclon(client, userdata, msg,):
 	conn1 =pymysql.connect(database="OceanNet",user="admin",password="admin",host="localhost")
 	cur1=conn1.cursor()
-	payload=ast.literal_eval(msg.payload.decode())
-	print(type(payload))
+	payload=msg.payload.decode()
+	print(payload)
+	cur1.execute("INSERT INTO faclonRaw (data) VALUES (%d);",payload)
 # 	cur1.execute("INSERT INTO piggyback(TIME,boat,dir,ping_ms,ss,nf,rssi,pos,ccq,d,txrate,rxrate,freq,channel,bs_ip) VALUES(%(TIME)s,%(boat,%(dir)s,%(ping_ms)s,%(ss)s,%(nf)s,%(rssi)s,%(pos)s,%(ccq)s,%(d)s,%(txrate)s,%(rxrate)s,%(freq)s,%(channel)s,%(bs_ip)s);",payload)
 # 	conn1.commit()
 # 	conn1.close()
