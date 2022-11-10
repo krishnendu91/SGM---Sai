b3a7c4b03fc97b925d12b0da49f2955e58f9dffe
Sai Shibu N B <saishibu38380@gmail.com>
2022-11-10 14:28:44 +0530

    Create simpleNewsFetch.py

---
+++ 4Ward/simpleNewsFetch.py
@@ -0,0 +1,16 @@
+from serpapi import GoogleSearch
+
+params = {
+  "q": "Coffee",
+  "location": "Kochi, Kerala, India",
+  "hl": "en",
+  "gl": "en",
+  "google_domain": "google.co.in/",
+  "api_key": "a2c05cf3986b23c9078d8b57d283bd138460fa15b1f54c9f66aee3d8f4310240"
+  "nfpr": "1"
+  "tbm": "nws"
+}
+
+search = GoogleSearch(params)
+results = search.get_dict()
+print(results)
