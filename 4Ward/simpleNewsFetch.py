bc99b5a0ce32f7503f95574f79f52471972a9bdd
Sai Shibu N B <saishibu38380@gmail.com>
2022-11-10 14:34:34 +0530

    Update simpleNewsFetch.py

--- 4Ward/simpleNewsFetch.py
+++ 4Ward/simpleNewsFetch.py
@@ -4,7 +4,7 @@ params = {
   "q": "Coffee",
   "location": "Kochi, Kerala, India",
   "hl": "en",
-  "gl": "en",
+  "gl": "in",
   "google_domain": "google.co.in",
   "api_key": "a2c05cf3986b23c9078d8b57d283bd138460fa15b1f54c9f66aee3d8f4310240",
   "tbs": "nws"
