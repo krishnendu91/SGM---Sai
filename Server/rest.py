8cb66617e22bcf37284caf79c5fbf2f7b8ecc831
Sai Shibu N B <saishibu38380@gmail.com>
2022-11-26 17:49:29 +0530

    Update rest.py

--- Server/rest.py
+++ Server/rest.py
@@ -554,6 +554,14 @@ def embedosRaw():
 # 	r[0]['timestampEpoch']=r[0]['timestamp'].timestamp()*1000
 	return jsonify({'Recent data' : r})
 
+@app.route('/4ward/embedos')
+def embedos():
+	security(str(sys._getframe().f_code.co_name))
+	cur = mysql.connect().cursor()
+	cur.execute('select * from embedos ORDER BY id DESC LIMIT 1 ')
+	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+# 	r[0]['timestampEpoch']=r[0]['timestamp'].timestamp()*1000
+	return jsonify({'Recent data' : r})
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0",port=5000,debug=1)
