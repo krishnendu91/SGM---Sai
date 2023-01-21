3d261537d615ad953ce72761fd62f8b7faff4625
Sai Shibu N B <saishibu38380@gmail.com>
2023-01-21 17:24:55 +0530

    Update 4warderror.py

--- Server/4warderror.py
+++ Server/4warderror.py
@@ -14,8 +14,10 @@ def sendmessage(message):
 conn1 =pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
 cur1=conn1.cursor()
 
+
 cur1.execute("SELECT faclon1, wiman1, wiman2, vvm, embedos FROM `4wardDevStatus` ORDER BY ID DESC LIMIT 1;")
-print(cur1.fetchone())
+pastData = cur1.fetchone())
+status = {"faclon1":pastData[0],"wiman1":pastData[1],"wiman2":pastData[2], "vvm":pastData[3], "embedos":pastData[4]}
 
 #Faclon 869523055584990
 cur1.execute("SELECT receiveTime FROM `faclon` WHERE imei = 869523055584990 ORDER BY `faclon`.`receiveTime` DESC LIMIT 1;")
@@ -24,8 +26,9 @@ RT2 = time.mktime(RT.timetuple())
 currentTime = time.time()
 delta = (currentTime-RT2)
 if delta > 600:
-	sendmessage("Faclon - 869523055584990 - No data for last 10 min. Last data received time: " + str(RT) )
-	faclon1 = 1
+	if status["faclon1"] == 0:
+		sendmessage("Faclon - 869523055584990 - No data for last 10 min. Last data received time: " + str(RT) )
+		faclon1 = 1
 else:
 	faclon1 = 0
 
@@ -49,8 +52,9 @@ RT2 = time.mktime(RT.timetuple())
 currentTime = time.time()
 delta = (currentTime-RT2)
 if delta > 600:
-	sendmessage("Wiman - 869523057988207 - No data for last 10 min. Last data received time: " + str(RT) )
-	wiman2 = 1
+	if status["wiman2"] == 0:
+		sendmessage("Wiman - 869523057988207 - No data for last 10 min. Last data received time: " + str(RT) )
+		wiman2 = 1
 else:
 	wiman2 = 0
 
@@ -81,7 +85,8 @@ else:
 	
 
 status = {"faclon1":faclon1,"wiman1":wiman1,"wiman2":wiman2, "vvm":vvm, "embedos":embedos}
-cur1.execute("INSERT INTO `4wardDevStatus` (`faclon1`, `wiman1`, `wiman2`, `vvm`, `embedos`) VALUES (%(faclon1)s, %(wiman1)s, %(wiman2)s, %(vvm)s, %(embedos)s;",status)
+print(status)
+cur1.execute("INSERT INTO 4wardDevStatus (faclon1, wiman1, wiman2, vvm, embedos) VALUES (%(faclon1)s, %(wiman1)s, %(wiman2)s, %(vvm)s, %(embedos)s;",status)
 cur1.commit()
 conn1.close()
 
