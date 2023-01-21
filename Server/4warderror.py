445017189064c7d181fac5997186a2704a54ae4a
Sai Shibu N B <saishibu38380@gmail.com>
2023-01-21 16:58:06 +0530

    Update 4warderror.py

--- Server/4warderror.py
+++ Server/4warderror.py
@@ -13,12 +13,57 @@ def sendmessage(message):
 
 conn1 =pymysql.connect(database="AmritaSGM",user="admin",password="admin",host="localhost")
 cur1=conn1.cursor()
-cur1.execute("SELECT receiveTime FROM `faclon`  ORDER BY `faclon`.`receiveTime`  DESC LIMIT 1")
-faclonRT = time.mktime(cur1.fetchone()[0].timetuple())
+
+#Faclon 869523055584990
+cur1.execute("SELECT receiveTime FROM `faclon` WHERE imei = 869523055584990 ORDER BY `faclon`.`receiveTime` DESC LIMIT 1;")
+RT = cur1.fetchone()[0]
+RT2 = time.mktime(RT.timetuple())
 currentTime = time.time()
-print(faclonRT)
+print(RT)
 print(currentTime)
-print((currentTime-faclonRT)/60)
+delta = (currentTime-RT2)
+if delta > 600:
+	sendmessage("Faclon - 869523055584990 - No data for last 10 min. Last data received time: " + str(RT) )
+
+#Wiman 869523057988207
+cur1.execute("SELECT receiveTime FROM `wiman` WHERE imei = 869523057988207 ORDER BY `receiveTime` DESC LIMIT 1;")
+RT = cur1.fetchone()[0]
+RT2 = time.mktime(RT.timetuple())
+currentTime = time.time()
+delta = (currentTime-RT2)
+if delta > 600:
+	sendmessage("Wiman - 869523057988207 - No data for last 10 min. Last data received time: " + str(RT) )
+	
+
+#Wiman 869523057983679
+cur1.execute("SELECT receiveTime FROM `wiman` WHERE imei = 869523057983679 ORDER BY `receiveTime` DESC LIMIT 1;")
+RT = cur1.fetchone()[0]
+RT2 = time.mktime(RT.timetuple())
+currentTime = time.time()
+delta = (currentTime-RT2)
+if delta > 600:
+	sendmessage("Wiman - 869523057988207 - No data for last 10 min. Last data received time: " + str(RT) )
+
+
+#VVM AMGW001
+cur1.execute("SELECT receiveTime FROM `VVMGateway` WHERE devID = AMGW001 ORDER BY `receiveTime` DESC LIMIT 1;")
+RT = cur1.fetchone()[0]
+RT2 = time.mktime(RT.timetuple())
+currentTime = time.time()
+delta = (currentTime-RT2)
+if delta > 600:
+	sendmessage("VVM - AMGW001 - No data for last 10 min. Last data received time: " + str(RT) )
+
+#Embeddos	
+cur1.execute("SELECT timestamp FROM `embedos` WHERE devID = AMGW001 ORDER BY `receiveTime` DESC LIMIT 1;")
+RT = cur1.fetchone()[0]
+RT2 = time.mktime(RT.timetuple())
+currentTime = time.time()
+delta = (currentTime-RT2)
+if delta > 600:
+	sendmessage("Embeddos - No data for last 10 min. Last data received time: " + str(RT) )
+
+	
 
 conn1.close()
 
