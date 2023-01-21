fabbf22bf3aa599ba1fdc216befebdce795acbfa
Sai Shibu N B <saishibu38380@gmail.com>
2023-01-21 14:58:05 +0530

    Update 4warderror.py

--- Server/4warderror.py
+++ Server/4warderror.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import telegram,pymysql
-bot = telegram.Bot(token='813728431:AAEmpmT-UXIQQcdzwkih8k1XSdCbiMIFP2Q')
+import telebot
+bot = telebot.TeleBot(token='813728431:AAEmpmT-UXIQQcdzwkih8k1XSdCbiMIFP2Q')
 
 def sendmessage(message):
 	chat_id =-304438902
