e52b0c4fb656405ac36339c032cbd1ec2f372f1e
Sai Shibu N B <saishibu38380@gmail.com>
2023-02-09 17:20:49 +0530

    Update 4warderror.py

--- Server/4warderror.py
+++ Server/4warderror.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 bot = telebot.TeleBot(token='813728431:AAEmpmT-UXIQQcdzwkih8k1XSdCbiMIFP2Q')
 
 def sendmessage(message):
-	chat_id =-304438902
+	chat_id =-663842076
 	bot.send_message(chat_id=chat_id, text=message)
 	return "Done!"
 
