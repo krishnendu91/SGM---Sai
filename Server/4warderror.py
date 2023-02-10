6c87b39476cf95a5b524490bc1138c8e6fd4ac99
Sai Shibu N B <saishibu38380@gmail.com>
2023-02-10 09:21:56 +0530

    Update 4warderror.py

--- Server/4warderror.py
+++ Server/4warderror.py
@@ -17,6 +17,7 @@ wiman2=0
 
 cur1.execute("SELECT faclon1, wiman1, wiman2, vvm, embedos FROM `4wardDevStatus` ORDER BY ID DESC LIMIT 1;")
 pastData = cur1.fetchone()
+faclon1 = pastData[0]
 status = {"faclon1":pastData[0],"wiman1":pastData[1],"wiman2":pastData[2], "vvm":pastData[3], "embedos":pastData[4]}
 print(status)
 #Faclon 869523055584990
@@ -29,6 +30,8 @@ if delta > 600:
 	if status["faclon1"] == 0:
 		sendmessage("Faclon - 869523055584990 - No data for last 10 min. Last data received time: " + str(RT) )
 		faclon1 = 1
+	else:
+		faclon1 = 0
 else:
 	faclon1 = 0
 
@@ -39,8 +42,11 @@ RT2 = time.mktime(RT.timetuple())
 currentTime = time.time()
 delta = (currentTime-RT2)
 if delta > 600:
-	sendmessage("Wiman - 869523057988207 - No data for last 10 min. Last data received time: " + str(RT) )
-	wiman1 = 1
+	if status["wiman1"] == 0:
+		sendmessage("Wiman - 869523057988207 - No data for last 10 min. Last data received time: " + str(RT) )
+		wiman1 = 1
+	else:
+		wiman1 = 0
 else:
 	wiman1 = 0
 	
@@ -53,8 +59,10 @@ currentTime = time.time()
 delta = (currentTime-RT2)
 if delta > 600:
 	if status["wiman2"] == 0:
-		sendmessage("Wiman - 869523057988207 - No data for last 10 min. Last data received time: " + str(RT) )
+		sendmessage("Wiman - 869523057983679 - No data for last 10 min. Last data received time: " + str(RT) )
 		wiman2 = 1
+	else:
+		wiman2 = 0
 else:
 	wiman2 = 0
 
@@ -66,8 +74,11 @@ RT2 = time.mktime(RT.timetuple())
 currentTime = time.time()
 delta = (currentTime-RT2)
 if delta > 600:
-	sendmessage("VVM - AMGW001 - No data for last 10 min. Last data received time: " + str(RT) )
-	vvm = 1
+	if status["vvm"] == 0:
+		sendmessage("VVM - AMGW001 - No data for last 10 min. Last data received time: " + str(RT) )
+		vvm = 1
+	else:
+		vvm = 0
 else:
 	vvm = 0
 
@@ -78,8 +89,11 @@ RT2 = time.mktime(RT.timetuple())
 currentTime = time.time()
 delta = (currentTime-RT2)
 if delta > 600:
-	sendmessage("Embeddos - No data for last 10 min. Last data received time: " + str(RT) )
-	embedos = 1
+	if status["embedos"] == 0:
+		sendmessage("Embeddos - No data for last 10 min. Last data received time: " + str(RT) )
+		embedos = 1
+	else:
+		embedos = 0
 else:
 	embedos = 0
 	
